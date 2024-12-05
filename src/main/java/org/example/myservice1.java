ca67cb41ffd12a4cf269f76df44dbd4c2a4e2dde
Jayendra <sudhaacj@gmail.com>
2024-12-05 08:30:35 +0530

    commit

---
+++ src/main/java/org/example/myservice1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class myservice1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(myservice1.class, args);
+    }
+
+}
